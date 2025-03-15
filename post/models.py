e8d61dc3179b0b5d73a5ae5e9de90a75f71680b8
andrea <andreacduarteg@gmail.com>
2025-03-15 11:15:25 -0400

    add post app

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title #solo modifica el administrador de django
+    
+    # image = models.ImageField(upload_to='images/', blank=True, null=True)
--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from post.models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post.html'
+
+# def post(request):                            #BASADO EN FUNCIONES
+#    posts = Post.objects.all()
+
+#    context = {
+#        'posts': posts
+#   }
+
+#    return render(request, 'post.html', context)
